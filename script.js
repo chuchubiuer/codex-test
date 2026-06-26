b328950ad6226c68fcbf0b3badd776434e15b8ad
chuchubiuer <1453192834@qq.com>
2026-06-26 20:55:32 +0800

    update homepage

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const learnButton = document.querySelector("#learnButton");
+const celebrateButton = document.querySelector("#celebrateButton");
 const message = document.querySelector("#message");
 
-learnButton.addEventListener("click", () => {
-  message.textContent = "很好，今天也完成了一次网页练习。";
+celebrateButton.addEventListener("click", () => {
+  message.textContent = "网页已经成功上线啦！";
 });
